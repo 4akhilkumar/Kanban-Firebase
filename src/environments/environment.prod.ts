77487aef1b126e92e6ca9932311edc73b9f80483
Sai Akhil Kumar Reddy Nallamilli <4akhilkumar@gmail.com>
2021-05-25 16:49:03 +0530

    Initial Commit - Kanban-Firebase
    https://developers.google.com/codelabs/building-a-web-app-with-angular-and-firebase?utm_source=google-io&utm_medium=organic&utm_campaign=io21-learninglab#11

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: true,
+  firebase: {
+  apiKey: "AIzaSyC-DSmWsGgd5ALKBl0M-jV-Eh9Zua3H-Yg",
+  authDomain: "kanbanfire-12700.firebaseapp.com",
+  projectId: "kanbanfire-12700",
+  storageBucket: "kanbanfire-12700.appspot.com",
+  messagingSenderId: "408486393627",
+  appId: "1:408486393627:web:6f70fd02a297e694d245be",
+  measurementId: "G-F0TQ89SF38"
+  },
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,25 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: true,
+  firebase: {
+  apiKey: "AIzaSyC-DSmWsGgd5ALKBl0M-jV-Eh9Zua3H-Yg",
+  authDomain: "kanbanfire-12700.firebaseapp.com",
+  projectId: "kanbanfire-12700",
+  storageBucket: "kanbanfire-12700.appspot.com",
+  messagingSenderId: "408486393627",
+  appId: "1:408486393627:web:6f70fd02a297e694d245be",
+  measurementId: "G-F0TQ89SF38"
+  },
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
